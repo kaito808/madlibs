863d354d2118e752ea823d4a1126f40b12b24ea1
Kaito <107011546+kaito808@users.noreply.github.com>
2022-12-03 14:10:18 -1000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "mmmm"
+
+debug = DebugToolbarExtension(app)
+
+@app.route("/")
+def questions():
+    prompts = story.prompts
+    return render_template("questions.html", prompts=prompts)
+
+@app.route("/story")
+def story():
+    """Show story result."""
+
+    text = story.generate(request.args)
+
+    return render_template("story.html", text=text)    
